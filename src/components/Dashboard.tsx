f92b3d69cd20a8a107764b8808e41dcc7cfd00cf
Saif <faysalbakziz@gmail.com>
2023-10-10 22:27:39 +0100

    added the dynamic path for the files

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -52,7 +52,7 @@ const Dashboard = () => {
               >
                 <Link
                   className=" flex flex-col gap-2"
-                  href={`/dashboard/${file.id}`}
+                  href={`/dashboard/${file.id}`} 
                 >
                   <div className=" pt-6 px-6 flex w-full items-center justify-between space-x-6 ">
                     <div className=" h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
